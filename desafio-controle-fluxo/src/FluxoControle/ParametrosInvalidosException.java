b8fad08dacc28aafdf5db10b8e9c486cfc7ce904
Paulo Melo <paulomelo.adm@hotmail.com>
2025-07-30 22:53:21 -0300

    Atualizando repositório

---
+++ desafio-controle-fluxo/src/FluxoControle/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+package FluxoControle;
+
+public class ParametrosInvalidosException extends Exception {
+
+    // Exceção personalizada
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem); 
+    }
+}
